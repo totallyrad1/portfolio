1a6d1c38731869b4d35b608e6561fd1d96c063cb
Asaad Naji <assaadnaji014@gmail.com>
2024-10-14 14:35:14 +0100

    added 42-profile in the about me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,18 +3,25 @@ import Reveal from "./Reveal";
 const AboutMe = () => {
     return (<div className="aboutme-div overflow-hidden">
         <Reveal>
-            <h5 className=" text-4xl font-extrabold text-black animate-wiggle animate-wiggle animate-infinite animate-duration-[4000ms]">
+            <h5 className="pb-3 text-4xl font-extrabold text-black animate-wiggle animate-wiggle animate-infinite animate-duration-[4000ms]">
                     About ME
             </h5>
         </Reveal>
-        <Reveal>
-            <div className="aboutme-paragraph animate-pulse animate-infinite py-12">
-                <p>I’m <a href="https://www.linkedin.com/in/asaad-naji-90423429a/" target="blank"><strong>Asaad Naji </strong> or <strong>RaD</strong> </a>a 19-year-old Moroccan currently studying at 1337.</p>
-                <p>I’m passionate about coding and always eager to learn new things.</p>
-                <p>I enjoy taking on challenges and am quick to adapt and pick up new skills. </p>
-                <p>Whether it's solving problems or building projects, I’m driven by a desire to continuously grow as a developer.</p>
-            </div>
-        </Reveal>
+        <div className="grid lpl:grid-cols-2 grid-cols-1 justify-between">
+            <Reveal>
+                <div className="aboutme-paragraph text-center animate-pulse animate-infinite py-7">
+                    <p>I’m <a href="https://www.linkedin.com/in/asaad-naji-90423429a/" target="blank"><strong>Asaad Naji </strong> or <strong>RaD</strong> </a>a 19-year-old Moroccan currently studying at 1337.</p>
+                    <p>I’m passionate about coding and always eager to learn new things.</p>
+                    <p>I enjoy taking on challenges and am quick to adapt and pick up new skills. </p>
+                    <p>Whether it's solving problems or building projects, I’m driven by a desire to continuously grow as a developer.</p>
+                </div>
+            </Reveal>
+            <Reveal>
+                <div className="flex justify-center py-3">
+                    <a href="https://github.com/oakoudad/badge42"><img src="https://badge.mediaplus.ma/black/asnaji" alt="asnaji's 42 stats" /></a>
+                </div>
+            </Reveal>
+        </div>
     </div>);
 }
  
